09274a37ffdf084f571cd885b00ce0f6ef46332b
Melinda Ervin <melindaervin17@gmail.com>
2022-06-05 17:50:18 -0400

    Add Dev POCO

--- Src/Komodo.Data/Pocos/Developer.cs
+++ Src/Komodo.Data/Pocos/Developer.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-//namespace Pocos
-
     public class Developer
     {
-        
+        public Developer(){}
+        public Developer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Developer(bool hasPluralAccess)
+        {
+        if(hasPluralAccess == true)
+        {
+            System.Console.WriteLine("Access granted.");
+        }
+        else
+        {
+            System.Console.WriteLine("Access denied.");
+        }
+        }
+
+        public int ID { get; set }
+        public string FirstName { get; set }
+        public string LastName { get; set }
     }
