cfb4ba95466fa32bc9cf770a1fca3eb73871911d
Melinda Ervin <melindaervin17@gmail.com>
2022-06-08 19:39:36 -0400

    Add DevRepo

--- Src/Komodo.Repository/Repository/DevRepo.cs
+++ Src/Komodo.Repository/Repository/DevRepo.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-// namespace Repository
-// {
     public class DevRepo
     {
-        
+        private readonly List<Developer> _developerDirectory = new List<Developer>();
+		
+		private int _count;
+
+		public bool AddDeveloperToDirectory(Developer developer)
+		{
+			if(developer != null)
+			{
+				_count++;
+				developer.ID = _count;
+				_developerDirectory.Add(developer);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+
+		public List<Developer> GetAllDevelopers()
+		{
+			return _developerDirectory;
+		}
+
+		public Developer GetDeveloperByID(int id)
+		{
+			foreach(Developer d in _developerDirectory)
+			{
+				if(d.ID == id)
+				{
+					return d;
+				}
+			}
+
+			return null;
+		}
+
+		public List<Developer> PluralsightLicense();
+		{
+			var license = new List<Developer>();
+			foreach (Developer d in _developerDirectory)
+			{
+				if(d.PluralsightLicense == false)
+				{
+					license.Add(d);
+				}
+			}
+			return license;
+		}
     }
-// }
