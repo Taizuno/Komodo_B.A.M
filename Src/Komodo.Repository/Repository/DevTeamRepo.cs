e78975885c40aa460a860a04e6d62e28dc134b67
Braeden McNealy <lightning9501@gmail.com>
2022-06-08 20:08:53 -0400

    DevTeamRepoActual

--- Src/Komodo.Repository/Repository/DevTeamRepo.cs
+++ Src/Komodo.Repository/Repository/DevTeamRepo.cs
@@ -8,9 +8,61 @@ using System.Threading.Tasks;
     {
 
         private readonly List<DevTeam> _devTeamDirectory = new List<DevTeam>();
+           private int _count;
     // ? Create
-    // ?Read All
-    // ? Read by ID
-    // ? Update-By ID
-    // ? Delete-By ID
+        public bool AddTeamToDir(DevTeam team)
+        {
+            if(team != null)
+            {
+                _devTeamDirectory.Add(team);
+                return true;
+            }
+            return null;
         }
+    // ?Read All
+        public List<DevTeam> GetTeams()
+        {
+            return _devTeamDirectory;
+        }
+    // ? Read by ID
+        public DevTeam GetTeamByID(int id)
+        {
+            foreach(DevTeam dt in _devTeamDirectory)
+            {
+                if(dt.ID == id)
+                {
+                    return dt;
+                }
+            }
+            return null;
+        }
+    // ? Update-By ID
+        public bool UpdateTeamByID(int id, DevTeam newTeam)
+        {
+            var team = GetTeamByID(id);
+
+            if(team != null)
+            {
+                team.ID = newTeam.ID;
+                team.TeamName = newTeam.TeamName;
+                team.Developers = newTeam.Developers;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    // ? Delete-By ID
+        public bool RemoveTeamByID(int id)
+        {
+            var team = GetTeamByID(id);
+            if(team != null)
+            {
+                _devTeamDirectory.Remove(team);
+                return true;
+            }
+            else
+            {return false;}
+        }
+    }
